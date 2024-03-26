f61c215a5f4a950b46d18459e25dff29dc30d547
tbrekalo <t@tbrekalo.xyz>
2024-03-26 22:49:39 +0100

    works

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,4 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <cctype>
 #include <cstdio>
 
 #include "doctest/doctest.h"
@@ -7,20 +6,21 @@
 using namespace std::literals;
 
 static auto parse_expr(char const* s, double* d) -> int {
-  auto i = 0, n_cur = 0, n_next = 0, n_sign = 0, sign = 1;
+  auto i = 0, n_cur = 0, n_sign = 0, sign = 1;
   auto cur = 0., next = 0.;
-  for (*d = 0.; *(s + i) && *(s + i) != ')'; sign = 1, n_cur = n_next = 0) {
+  for (*d = 0.; *(s + i) && *(s + i) != ')'; sign = 1, n_cur = 0) {
     for (n_sign = 0; *(s + i + n_sign) == '+' || *(s + i + n_sign) == '-';
          ++n_sign)
       sign *= *(s + i + n_sign) == '-' ? -1. : 1.;
     if (sscanf(s + i + n_sign, "%lf%n", &cur, &n_cur); n_cur == 0)
       n_cur = parse_expr(s + i + n_sign + 1, &cur) + 2;
     i += n_sign + n_cur;
-    for (auto c = s + i; *c && *c != ')' && *c != '+' && *c != '-'; c = s + i) {
-      for (auto q = c; !std::isdigit(*q) && *q != '('; ++q) ++n_next;
-      n_next += parse_expr(c + !std::isdigit(*c), &next) + 2 * (*c == '(');
+    for (auto q = s, c = s + i; *c && *c != ')' && *c != '+' && *c != '-';
+         c = s + i) {
+      for (q = c; *q && (*q == '*' || *q == '/'); ++q)
+        ;
+      i += (q - c) + parse_expr(q + (*q == '('), &next) + 2 * (*q == '(');
       cur = *c != '/' ? cur * next : cur / next;
-      i += n_next + (*c == '*' || *c == '/');
     }
     *d += sign * cur;
   }
@@ -35,26 +35,26 @@ TEST_CASE("parse_expr") {
     return ret;
   };
 
-  // CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
-  // CHECK_EQ(parser("1+1"), std::tuple{Approx(2.), 3});
-  // CHECK_EQ(parser("1++1"), std::tuple{Approx(2.), 4});
-  // CHECK_EQ(parser("1++-1"), std::tuple{Approx(0.), 5});
-  // CHECK_EQ(parser("(1)"), std::tuple{Approx(1.), 3});
-  // CHECK_EQ(parser("((1))"), std::tuple{Approx(1.), 5});
-  // CHECK_EQ(parser("(1+1)"), std::tuple{Approx(2.), 5});
-  // CHECK_EQ(parser("(1)+(1)"), std::tuple{Approx(2.), 7});
-  // CHECK_EQ(parser("((1))+(1)"), std::tuple{Approx(2.), 9});
-  // CHECK_EQ(parser("((1))+-+(1)"), std::tuple{Approx(0.), 11});
+  CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("1+1"), std::tuple{Approx(2.), 3});
+  CHECK_EQ(parser("1++1"), std::tuple{Approx(2.), 4});
+  CHECK_EQ(parser("1++-1"), std::tuple{Approx(0.), 5});
+  CHECK_EQ(parser("(1)"), std::tuple{Approx(1.), 3});
+  CHECK_EQ(parser("((1))"), std::tuple{Approx(1.), 5});
+  CHECK_EQ(parser("(1+1)"), std::tuple{Approx(2.), 5});
+  CHECK_EQ(parser("(1)+(1)"), std::tuple{Approx(2.), 7});
+  CHECK_EQ(parser("((1))+(1)"), std::tuple{Approx(2.), 9});
+  CHECK_EQ(parser("((1))+-+(1)"), std::tuple{Approx(0.), 11});
 
-  // CHECK_EQ(parser("1(1)"), std::tuple{Approx(1.), 4});
-  // CHECK_EQ(parser("(1)1"), std::tuple{Approx(1.), 4});
-  // CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 6});
-  // CHECK_EQ(parser("(1)/(1)"), std::tuple{Approx(1.), 7});
-  // CHECK_EQ(parser("(2)*(1.5)"), std::tuple{Approx(3.), 9});
-  // CHECK_EQ(parser("2(1+1)1/1"), std::tuple{Approx(4.), 9});
-  // CHECK_EQ(parser("2(1+1)1/-1"), std::tuple{Approx(-4.), 10});
-  // CHECK_EQ(parser("2(1+1)-1/-1"), std::tuple{Approx(5.), 11});
-  // CHECK_EQ(parser("2(1+(1))-1/-1"), std::tuple{Approx(5.), 13});
-  // CHECK_EQ(parser("2((1+(1)))-1/-1"), std::tuple{Approx(5.), 15});
+  CHECK_EQ(parser("1(1)"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("(1)1"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 6});
+  CHECK_EQ(parser("(1)/(1)"), std::tuple{Approx(1.), 7});
+  CHECK_EQ(parser("(2)*(1.5)"), std::tuple{Approx(3.), 9});
+  CHECK_EQ(parser("2(1+1)1/1"), std::tuple{Approx(4.), 9});
+  CHECK_EQ(parser("2(1+1)1/-1"), std::tuple{Approx(-4.), 10});
+  CHECK_EQ(parser("2(1+1)-1/-1"), std::tuple{Approx(5.), 11});
+  CHECK_EQ(parser("2(1+(1))-1/-1"), std::tuple{Approx(5.), 13});
+  CHECK_EQ(parser("2((1+(1)))-1/-1"), std::tuple{Approx(5.), 15});
   CHECK_EQ(parser("2*((1+(1)))-1/-1"), std::tuple{Approx(5.), 16});
 }
