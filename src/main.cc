1642f470b77e65c61d7feb5f3b237952f55f822c
tbrekalo <t@tbrekalo.xyz>
2024-03-26 20:54:16 +0100

    working simple

--- src/main.cc
+++ src/main.cc
@@ -5,20 +5,25 @@
 
 using namespace std::literals;
 
-static auto parse_digits(char const* s, double* d) -> int {
-  auto sign = 1, n = 0, i = 0;
-  for (sscanf(s, "%lf%n", d, &n); *s && !n; sscanf(s + (++i), "%lf%n", d, &n))
-    sign *= (*(s + i) == '-') ? -1. : 1.;
+static int parse_sign(char const* s, int* d) {
+  int i = 0;
+  for (*d = 1; *(s + i) == '+' || *(s + i) == '-'; ++i)
+    *d *= *(s + i) == '-' ? -1. : 1.;
+  return i;
+};
+
+static auto parse_numeric(char const* s, double* d) -> int {
+  int sign, n, i = parse_sign(s, &sign);
+  int f = sscanf(s + i, "%lf%n", d, &n);
   *d *= sign;
-  return i + n;
+  return f * (i + n);
 }
 
-TEST_CASE("parse_digits") {
+TEST_CASE("parse_numeric") {
   using doctest::Approx;
-  auto parser =
-      [x = 0.](std::string_view str) mutable -> std::tuple<double, int> {
-    auto ret = std::tuple{x, parse_digits(str.data(), &x)};
-    return ret;
+  auto parser = [](std::string_view str) -> std::tuple<double, int> {
+    auto x = 0.;
+    return std::tuple{x, parse_numeric(str.data(), &x)};
   };
 
   CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
@@ -42,18 +47,20 @@ TEST_CASE("parse_digits") {
 }
 
 static auto parse_expr(char const* s, double* d) -> int {
-  auto i = 0, n = 0;
-  auto buff_curr = 0., buff_next = 0.;
-  for (*d = 0.; *(s + i) && *(s + i) != ')';) {
-    if (*(s + i) == '(') {
-      n = parse_expr(s + i + 1, &buff_curr) + 2;
-    } else {
-      n = parse_digits(s + i, &buff_curr);
+  auto i = 0, n_cur = 0, n_next = 0, n_sign = 0, sign = 1;
+  auto cur = 0., next = 0.;
+  for (*d = 0.; *(s + i) && *(s + i) != ')'; sign = 1) {
+    n_sign = parse_sign(s + i, &sign);
+    if (sscanf(s + i + n_sign, "%lf%n", &cur, &n_cur); n_cur == 0)
+      n_cur = parse_expr(s + i + 1, &cur) + 2;
+    i += n_sign + n_cur;
+    for (auto c = s + i; *c && *c != ')' && *c != '+' && *c != '-'; c = s + i) {
+      n_next = parse_expr(c + (*c == '*' || *c == '/'), &next);
+      cur = *c != '/' ? cur * next : cur / next;
+      i += n_next + (*c == '*' || *c == '/');
     }
-    *d += buff_curr;
-    i += n;
+    *d += sign * cur;
   }
-
   return i;
 }
 
@@ -71,8 +78,10 @@ TEST_CASE("parse_expr") {
   CHECK_EQ(parser("1++-1"), std::tuple{Approx(0.), 5});
   CHECK_EQ(parser("(1)"), std::tuple{Approx(1.), 3});
   CHECK_EQ(parser("(1+1)"), std::tuple{Approx(2.), 5});
-  CHECK_EQ(parser("(1)+(1)"), std::tuple{Approx(2.), 7}); // TODO: continue
+  CHECK_EQ(parser("(1)+(1)"), std::tuple{Approx(2.), 7});  // TODO: continue
 
-  // CHECK_EQ(parser("1(1)"), std::tuple{Approx(1.), 1});
-  // CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("1(1)"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("(1)1"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 6});
+  CHECK_EQ(parser("(1)/(1)"), std::tuple{Approx(1.), 7});
 }
