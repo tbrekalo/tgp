a0a8acd4d0cc1d88ed472ddbbc0b59e2b319be0e
tbrekalo <t@tbrekalo.xyz>
2024-03-26 10:46:24 +0100

    update interface

--- src/main.cc
+++ src/main.cc
@@ -1,56 +1,68 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <cstdio>
-#include <string_view>
 
 #include "doctest/doctest.h"
 
 using namespace std::literals;
 
-static auto parse_digits(char const* s, double* d) -> char const* {
-  auto sign = 1, n = 0;
-  for (sscanf(s, "%lf%n", d, &n); *s && !n; sscanf(++s, "%lf%n", d, &n))
-    sign *= (*s == '-') ? -1. : 1.;
+static auto parse_digits(char const* s, double* d) -> int {
+  auto sign = 1, n = 0, i = 0;
+  for (sscanf(s, "%lf%n", d, &n); *s && !n; sscanf(s + (++i), "%lf%n", d, &n))
+    sign *= (*(s + i) == '-') ? -1. : 1.;
   *d *= sign;
-  return s + n;
+  return i + n;
 }
 
 TEST_CASE("parse_digits") {
   using doctest::Approx;
-  auto parser = [x = 0.](std::string_view str) mutable
-      -> std::tuple<double, std::string_view> {
+  auto parser =
+      [x = 0.](std::string_view str) mutable -> std::tuple<double, int> {
     auto ret = std::tuple{x, parse_digits(str.data(), &x)};
     return ret;
   };
 
-  CHECK_EQ(parser("1"), std::tuple{Approx(1.), ""});
-  CHECK_EQ(parser("-1"), std::tuple{Approx(-1.), ""});
-  CHECK_EQ(parser("1-1"), std::tuple{Approx(1.), "-1"});
-  CHECK_EQ(parser("-1-1"), std::tuple{Approx(-1.), "-1"});
-  CHECK_EQ(parser("--1"), std::tuple{Approx(1.), ""});
-  CHECK_EQ(parser("---1"), std::tuple{Approx(-1.), ""});
-  CHECK_EQ(parser("--+-1"), std::tuple{Approx(-1.), ""});
-  CHECK_EQ(parser("-+-1"), std::tuple{Approx(1.), ""});
-  CHECK_EQ(parser("-+-1+1"), std::tuple{Approx(1.), "+1"});
-  CHECK_EQ(parser("-+-1-1"), std::tuple{Approx(1.), "-1"});
+  CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("-1"), std::tuple{Approx(-1.), 2});
+  CHECK_EQ(parser("1-1"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("-1-1"), std::tuple{Approx(-1.), 2});
+  CHECK_EQ(parser("--1"), std::tuple{Approx(1.), 3});
+  CHECK_EQ(parser("---1"), std::tuple{Approx(-1.), 4});
+  CHECK_EQ(parser("--+-1"), std::tuple{Approx(-1.), 5});
+  CHECK_EQ(parser("-+-1"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("-+-1+1"), std::tuple{Approx(1.), 4});
+  CHECK_EQ(parser("-+-1-1"), std::tuple{Approx(1.), 4});
 
-  CHECK_EQ(parser("1."), std::tuple{Approx(1.), ""});
-  CHECK_EQ(parser("-1."), std::tuple{Approx(-1.), ""});
-  CHECK_EQ(parser("1.-1."), std::tuple{Approx(1.), "-1."});
-  CHECK_EQ(parser("-.1-.1"), std::tuple{Approx(-.1), "-.1"});
-  CHECK_EQ(parser("--1.0"), std::tuple{Approx(1.), ""});
-  CHECK_EQ(parser("--1..0"), std::tuple{Approx(1.), ".0"});
+  CHECK_EQ(parser("1."), std::tuple{Approx(1.), 2});
+  CHECK_EQ(parser("-1."), std::tuple{Approx(-1.), 3});
+  CHECK_EQ(parser("1.-1."), std::tuple{Approx(1.), 2});
+  CHECK_EQ(parser("-.1-.1"), std::tuple{Approx(-.1), 3});
+  CHECK_EQ(parser("--1.0"), std::tuple{Approx(1.), 5});
+  CHECK_EQ(parser("--1..0"), std::tuple{Approx(1.), 4});
 }
 
-static auto parse_expr(char const* s, double *d) -> char const* {
+static auto parse_expr(char const* s, double* d) -> int {
+  auto i = 0, n = 0;
+  auto buff = 0.;
+  for (n = parse_digits(s + 1, &buff); *s;) {
+  }
 
+  return i;
 }
 
 TEST_CASE("parse_expr") {
   using doctest::Approx;
-  auto parser = [x = 0.](std::string_view str) mutable
-      -> std::tuple<double, std::string_view> {
+  auto parser =
+      [x = 0.](std::string_view str) mutable -> std::tuple<double, int> {
     auto ret = std::tuple{x, parse_expr(str.data(), &x)};
     return ret;
   };
 
+  CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("(1)"), std::tuple{Approx(1.), 3});
+  CHECK_EQ(parser("1+1"), std::tuple{Approx(1.), 3});
+  CHECK_EQ(parser("(1+1)"), std::tuple{Approx(1.), 5});
+  CHECK_EQ(parser("(1)+(1)"), std::tuple{Approx(1.), 7});
+
+  CHECK_EQ(parser("1(1)"), std::tuple{Approx(1.), 1});
+  CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 1});
 }
