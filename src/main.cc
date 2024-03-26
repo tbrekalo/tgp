a8fab1da8d454f6031989e78d0f899d6f8a2ea27
tbrekalo <t@tbrekalo.xyz>
2024-03-26 21:04:52 +0100

    cleanup

--- src/main.cc
+++ src/main.cc
@@ -5,52 +5,13 @@
 
 using namespace std::literals;
 
-static int parse_sign(char const* s, int* d) {
-  int i = 0;
-  for (*d = 1; *(s + i) == '+' || *(s + i) == '-'; ++i)
-    *d *= *(s + i) == '-' ? -1. : 1.;
-  return i;
-};
-
-static auto parse_numeric(char const* s, double* d) -> int {
-  int sign, n, i = parse_sign(s, &sign);
-  int f = sscanf(s + i, "%lf%n", d, &n);
-  *d *= sign;
-  return f * (i + n);
-}
-
-TEST_CASE("parse_numeric") {
-  using doctest::Approx;
-  auto parser = [](std::string_view str) -> std::tuple<double, int> {
-    auto x = 0.;
-    return std::tuple{x, parse_numeric(str.data(), &x)};
-  };
-
-  CHECK_EQ(parser("1"), std::tuple{Approx(1.), 1});
-  CHECK_EQ(parser("+1"), std::tuple{Approx(1.), 2});
-  CHECK_EQ(parser("-1"), std::tuple{Approx(-1.), 2});
-  CHECK_EQ(parser("1-1"), std::tuple{Approx(1.), 1});
-  CHECK_EQ(parser("-1-1"), std::tuple{Approx(-1.), 2});
-  CHECK_EQ(parser("--1"), std::tuple{Approx(1.), 3});
-  CHECK_EQ(parser("---1"), std::tuple{Approx(-1.), 4});
-  CHECK_EQ(parser("--+-1"), std::tuple{Approx(-1.), 5});
-  CHECK_EQ(parser("-+-1"), std::tuple{Approx(1.), 4});
-  CHECK_EQ(parser("-+-1+1"), std::tuple{Approx(1.), 4});
-  CHECK_EQ(parser("-+-1-1"), std::tuple{Approx(1.), 4});
-
-  CHECK_EQ(parser("1."), std::tuple{Approx(1.), 2});
-  CHECK_EQ(parser("-1."), std::tuple{Approx(-1.), 3});
-  CHECK_EQ(parser("1.-1."), std::tuple{Approx(1.), 2});
-  CHECK_EQ(parser("-.1-.1"), std::tuple{Approx(-.1), 3});
-  CHECK_EQ(parser("--1.0"), std::tuple{Approx(1.), 5});
-  CHECK_EQ(parser("--1..0"), std::tuple{Approx(1.), 4});
-}
-
 static auto parse_expr(char const* s, double* d) -> int {
   auto i = 0, n_cur = 0, n_next = 0, n_sign = 0, sign = 1;
   auto cur = 0., next = 0.;
   for (*d = 0.; *(s + i) && *(s + i) != ')'; sign = 1) {
-    n_sign = parse_sign(s + i, &sign);
+    for (n_sign = 0; *(s + i + n_sign) == '+' || *(s + i + n_sign) == '-';
+         ++n_sign)
+      sign *= *(s + i + n_sign) == '-' ? -1. : 1.;
     if (sscanf(s + i + n_sign, "%lf%n", &cur, &n_cur); n_cur == 0)
       n_cur = parse_expr(s + i + 1, &cur) + 2;
     i += n_sign + n_cur;
@@ -84,4 +45,6 @@ TEST_CASE("parse_expr") {
   CHECK_EQ(parser("(1)1"), std::tuple{Approx(1.), 4});
   CHECK_EQ(parser("(1)(1)"), std::tuple{Approx(1.), 6});
   CHECK_EQ(parser("(1)/(1)"), std::tuple{Approx(1.), 7});
+  CHECK_EQ(parser("(2)*(1.5)"), std::tuple{Approx(3.), 9});
+  CHECK_EQ(parser("2(1+1)1/1"), std::tuple{Approx(4.), 9});
 }
